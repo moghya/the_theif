181eb731e9930b1967a88a98dae62f3c9e0a46fd
moghya <sawantshubham571@gmail.com>
2017-02-19 12:36:52 +0530

    implemented json thing
    works well

--- the_theif/theTheif.py
+++ the_theif/theTheif.py
@@ -4,32 +4,70 @@ from bs4 import BeautifulSoup
 
 s = requests.session()
 
+myusername = 'S1032140308'
+mypassword = '4Simple plan'
 
-def login(url):
-	page = s.get(url)
-
-	data = {
-		'ScriptManager1':'UpdatePanel1|btnLogin',
-		'__LASTFOCUS':'',
-		'__EVENTTARGET':'btnLogin',
-		'__EVENTARGUMENT':'',
-		'__VIEWSTATE':'/wEPDwUINDA4MDU2ODdkZIaeaNgZ0q5fBAzVXtd4eZPwo24o',
-		'__VIEWSTATEGENERATOR':'B8B84CAE',
-		'hdnMsg':'',
-		'txtUserId':'S1032140308',
-		'txtPassword':'4Simple plan',
-		'__ASYNCPOST':True
-	}
-	print(s.post(url,data))
-
+def login(url,username,password):
+    page = s.get(url)
+    data = {
+        'ScriptManager1':'UpdatePanel1|btnLogin',
+        '__LASTFOCUS':'',
+        '__EVENTTARGET':'btnLogin',
+        '__EVENTARGUMENT':'',
+        '__VIEWSTATE':'/wEPDwUINDA4MDU2ODdkZIaeaNgZ0q5fBAzVXtd4eZPwo24o',
+        '__VIEWSTATEGENERATOR':'B8B84CAE',
+        'hdnMsg':'',
+        'txtUserId':username,
+        'txtPassword':password,
+        '__ASYNCPOST':True
+    }
+    page = s.post(url,data)
+    return
 
 def testing_tables():
-	url = 'http://117.211.100.78/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN'
-	table_page = s.get(url)
-	plain_text = table_page.text
-	soup = BeautifulSoup(plain_text,'html.parser')
+    sdata = {}
+    attendance = {}
+    url = 'http://117.211.100.78/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN'
+    table_page = s.get(url)
+    plain_text = table_page.text
+    soup = BeautifulSoup(plain_text,'html.parser')
+    name = soup.find('div',{'class':'caption','id':'dvheader'})
+    if name:
+        data = str(name.text).split()
+        sdata['fname'] = data[4]
+        sdata['mname'] = data[5]
+        sdata['lname'] = data[6]
+        sdata['class'] = data[7]
+    table = soup.findAll('table')
+    print(table)
+    if len(table)>2:
+        table = table[3]
+        rows = table.findAll('tr')
+        courseName = ''
+        for i in range(1,len(rows)-4):
+            cols = rows[i].findAll('td')
+            if len(cols) == 6:
+                courseName = cols[1].text
+            subjectName = courseName + '__' + cols[-4].text
+            subType = cols[-4].text
+            present = cols[-3].text
+            total = cols[-2].text
+            percentage = cols[-1].text
+            sub = {
+                'present':present,
+                'total':total,
+                'percentage':percentage,
+                'subType': subType
+            }
+            if subjectName not in attendance:
+                attendance[subjectName] = sub
+        sdata['attendance'] = attendance
+    print(table)
+    return sdata
 
 
-def workForMe():
-	login('http://117.211.100.78/AdminLogin.aspx')
-	testing_tables()
+def getAttendance(username,password):
+    login('http://117.211.100.78/AdminLogin.aspx',username,password)
+    data = testing_tables()
+    print(json.dumps(data,indent=4))
+    return data
--- the_theif/views.py
+++ the_theif/views.py
@@ -3,9 +3,24 @@ from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.core.urlresolvers import reverse
+import json
 from . import theTheif
 
 def index(request):
-
-    response = ""
+    response = 'this is index page visit attendance page.'
     return HttpResponse(response)
+
+def getattendance(request):
+    username = request.GET.get('username',None)
+    password = request.GET.get('password',None)
+    if username and password:
+        data = theTheif.getAttendance(username,password)
+        response = json.dumps(data,indent=4)
+        context = {
+            'data':data
+        }
+        return render(request, 'attendance.html', context)
+        #return HttpResponse(response)
+    else:
+        return HttpResponse('I need Username and Password.')
+
