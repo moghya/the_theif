12b123fce24f7afc185cf69f62fe6c43205f6a09
Saurabh Banore <banoresaurabh@gmail.com>
2017-04-11 19:48:13 +0530

    Added new theTheif.py

--- the_theif/theTheif.py
+++ the_theif/theTheif.py
@@ -4,70 +4,82 @@ from bs4 import BeautifulSoup
 
 s = requests.session()
 
-myusername = 'S1032140308'
-mypassword = '4Simple plan'
+def login(url):
+	print('in login')
+	page = s.get(url)
+	data = {
+		'ScriptManager1':'UpdatePanel1|btnLogin',
+		'hdnMsg':'',
+		'hdtype':'',
+		'hdloginid':'',
+		'ddldatabasename':'cas',
+		'txtUserId':'s1032140308',
+		'txtPassword':'4Simple plan',
+		'__LASTFOCUS':'',
+		'__EVENTTARGET':'btnLogin',
+		'__EVENTARGUMENT':'',
+		'__VIEWSTATE':'/wEPDwULLTE1Mzc4NDYyNzcPFgIeDkxPR0lOX0JBU0VEX09OBQpMT0dJTl9OQU1FFgICAw9kFgICAw9kFgJmD2QWCgIHDxBkDxYBZhYBEAUDY2FzBQNjYXNnZGQCCQ8PFgIeBFRleHQFC3MxMDMyMTQwMzA4ZGQCCw8PFgIeB0VuYWJsZWRnZGQCDQ9kFgICAg9kFgICAQ8QZGQWAWZkAg8PDxYCHwJnZGRkZf0zrcOarLjpFbu4gUL//IQYCCoqTNiqQwfrnTMDIIo=',
+		'__VIEWSTATEGENERATOR':'B8B84CAE',
+		'__ASYNCPOST':True
 
-def login(url,username,password):
-    page = s.get(url)
-    data = {
-        'ScriptManager1':'UpdatePanel1|btnLogin',
-        '__LASTFOCUS':'',
-        '__EVENTTARGET':'btnLogin',
-        '__EVENTARGUMENT':'',
-        '__VIEWSTATE':'/wEPDwUINDA4MDU2ODdkZIaeaNgZ0q5fBAzVXtd4eZPwo24o',
-        '__VIEWSTATEGENERATOR':'B8B84CAE',
-        'hdnMsg':'',
-        'txtUserId':username,
-        'txtPassword':password,
-        '__ASYNCPOST':True
-    }
-    page = s.post(url,data)
-    return
+	}
+	page = s.post(url,data)
+	print(page.status_code)
+
 
 def testing_tables():
-    sdata = {}
-    attendance = {}
-    url = 'http://117.211.100.78/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN'
-    table_page = s.get(url)
-    plain_text = table_page.text
-    soup = BeautifulSoup(plain_text,'html.parser')
-    name = soup.find('div',{'class':'caption','id':'dvheader'})
-    if name:
-        data = str(name.text).split()
-        sdata['fname'] = data[4]
-        sdata['mname'] = data[5]
-        sdata['lname'] = data[6]
-        sdata['class'] = data[7]
-    table = soup.findAll('table')
-    print(table)
-    if len(table)>2:
-        table = table[3]
-        rows = table.findAll('tr')
-        courseName = ''
-        for i in range(1,len(rows)-4):
-            cols = rows[i].findAll('td')
-            if len(cols) == 6:
-                courseName = cols[1].text
-            subjectName = courseName + '__' + cols[-4].text
-            subType = cols[-4].text
-            present = cols[-3].text
-            total = cols[-2].text
-            percentage = cols[-1].text
-            sub = {
-                'present':present,
-                'total':total,
-                'percentage':percentage,
-                'subType': subType
-            }
-            if subjectName not in attendance:
-                attendance[subjectName] = sub
-        sdata['attendance'] = attendance
-    print(table)
-    return sdata
+	print('in testing')
+	url = 'http://117.211.100.78/STUDENT/SelfAttendence.aspx?MENU_CODE=MWEBSTUATTEN_SLF_ATTEN'
+	table_page = s.get(url)
+	print(table_page.status_code)
+	plain_text = table_page.text
+	soup = BeautifulSoup(plain_text,'html.parser')
+	
+	data = {}
 
+	try:
+		table = soup.findAll('table')[3]
+		subject_name = ""
+		i = 0
+		for tr in table.findAll('tr'):
+			if i != 0:
+				child_count = len(tr.findAll('td'))
+				td = tr.findAll('td')
+				if  child_count == 6:
+					td = tr.findAll('td')
+					subject_name = td[1].text
+					data[subject_name] = {}
+					data[subject_name][td[2].text] = {
+						"present" : int(td[3].text.strip()),
+						"total" : int(td[4].text.strip()),
+						"percentage" : float(td[5].text.strip())
+					}
+				elif child_count == 4:
+					cap_or_not = td[0].text.strip()
+					if cap_or_not[-1].isupper() == True:
+						data[subject_name][td[0].text] = {
+							"present" : int(td[1].text.strip()),
+							"total" : int(td[2].text.strip()),
+							"percentage" : float(td[3].text.strip())
+						}
+			else:
+				i += 1
+		data['VERDICT'] = {}
+		tr = table.findAll('tr')
+		for i in range(len(tr)-1,12,-1):
+			td = tr[i].findAll('td')
+			data["VERDICT"][td[0].text.strip()] = {
+				"present" : int(td[1].text.strip()),
+				"total" : int(td[2].text.strip()),
+				"percentage" : float(td[3].text.strip())
+			}
+		print(json.dumps(dict(data),indent=4))
+	except:
+		raise
+		print("There's some shitty problem going on with caserp.jnec.org !!!")
+	
+def workForMe():
+	login('http://117.211.100.78/AdminLogin.aspx')
+	testing_tables()
 
-def getAttendance(username,password):
-    login('http://117.211.100.78/AdminLogin.aspx',username,password)
-    data = testing_tables()
-    print(json.dumps(data,indent=4))
-    return data
+workForMe()
